f53c63ff18f9d7e008f04fb57c05432fb9ec0c2e
Ioramka <ana.ioramashvili.1@btu.edu.ge>
2024-06-12 09:37:49 +0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ana on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
